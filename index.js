d05a7de1744274821a531631aea0c629328c4356
pushpakjha07 <161748915+pushpakjha07@users.noreply.github.com>
2024-04-16 16:37:18 +0530

    index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Update Out V@1.5`, type: ActivityType.Watching }],
+      activities: [{ name: `LITE X CHEATS`, type: ActivityType.Playing }],
       status: 'idle',
     });
   }, 2000); 
